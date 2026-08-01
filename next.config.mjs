3825c409ef4bac5de721271f4fee6a1f83482720
Sweeney Ngo <sweeneyngo@proton.me>
2026-08-01 03:47:43 -0700

    chore: scaffold Next.js 15 + TypeScript project

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Allow remote product images from any storefront in <Image>-less <img> tags.
+  // We render plain <img>, so no domain allowlist is needed here.
+};
+
+export default nextConfig;
